99acdbed2a8cb09e1c15b558d8391fc21b7432e8
Adimora Chidozie <macflacco@gmail.com>
2019-03-24 22:23:50 +0100

    added alphamac.js

---
+++ alphamac.js
@@ -0,0 +1,22 @@
+
+
+ /*var stringToShout = prompt("Hello, Subscribe to our newsletter to get updates on our policies and terms and conditions, type in your email address.");
+ var shout = stringToShout.toUpperCase();
+ shout += "  WELCOME TO ALPHAMAC!!";
+ alert(shout);
+ alert('Before investing make sure to read and agree to our terms and services');*/
+
+alert(<h3><div class="form" >First name:<br>
+  <input type="text" name="firstname"><br>
+  Last name:<br>
+  <input type="text" name="lastname"><br>
+  Date of birth:<br>
+  <input type="text" name="Date of Birth"><br>
+  Phone Number:<br>
+  <input type="text" name="Phone Number"><br>
+  Email Address:<br>
+  <input type="text" name="Email Address"><br>
+  Country:<br>
+  <input type="text" name="Country"><br>
+  Postal Code:<br>
+  <input type="text" name="Postal Code"><br></div></h3>)
