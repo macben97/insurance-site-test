c19069edcc98883a832d6009bc6b3889dce4e791
Adimora Chidozie <macflacco@gmail.com>
2019-03-25 00:56:24 +0100

    added javascript.js

---
+++ javascript.js
@@ -0,0 +1,7 @@
+alert('hello this is mac');
+//this is a statement, used for display alerts
+document.write('write your message here');
+//used for writing directly to a webpage
+var message ="ALPHAMAC, GATEWAY TO FINANCIAL FREEDOM";
+alert(message);
+//the variable's "var" content(message) which is "ALPHAMAC, GATEWAY TO FINANCIAL FREEDOM"; appears on the dialog box  
